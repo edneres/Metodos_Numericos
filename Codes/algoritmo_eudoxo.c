a4d2fced206dcb2d44b0e94d3d2ee64f80921814
Edivânia NS <edneres7@gmail.com>
2023-10-20 13:24:32 -0300

    Initial commit

---
+++ Codes/algoritmo_eudoxo.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float algoritmo_de_eudoxo(double num)
+{
+    double aux_anterior, aux_atual;
+
+    for(int i = 0; i < 7; i++)
+    {
+        if(i == 0)
+        {
+            aux_atual = (1 + num) / 2;
+            aux_anterior = aux_atual;
+        }
+
+        else
+        {
+            aux_atual = (aux_anterior + (num / aux_anterior)) / 2;
+            aux_anterior = aux_atual;
+        }
+    }
+
+    /*
+    double x = 0.5 * (1 + num)
+
+    for (int i = 1; i <= 7; ++i)
+        x = 0.5 * (x + num / x); 
+    */
+
+    return aux_atual;
+}
+
+int main()
+{
+    system("cls");
+    double num = -1;
+
+    while (num < 1)
+    {
+        printf("Digite um numero real positivo: ");
+        scanf("%lf", &num);
+    }
+    
+    printf("\nRaiz quadrada de %.3lf: %lf\n\n", num, algoritmo_de_eudoxo(num));
+
+    system("pause");
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Metodos_Numericos
+ Codes in C of various Number Methods (Numerical Calculations) developed for the homonymous subject of the Electrical Engineering course at the Federal University of Piauí (2023).
